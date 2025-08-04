0ac798c361eab091e52e573b48a375d48b66f498
mvsoom <mvsoom@gmail.com>
2025-08-04 10:08:40 +0200

    Test run on IKLP Openglot data

---
+++ experiments/iklp-openglot/config.py
@@ -0,0 +1,22 @@
+# %%
+import glob
+import os
+from itertools import product
+from random import Random
+
+rng = Random(3082025)
+
+
+def wav_files():
+    PROJECT_DATA_PATH = os.environ["PROJECT_DATA_PATH"]
+    root_path = f"{PROJECT_DATA_PATH}/OPENGLOT/RepositoryI"
+    paths = glob.glob(f"{root_path}/**/*.wav", recursive=True)
+    return paths
+
+
+def configurations():
+    for (wav_file,) in product(wav_files()):
+        yield {
+            "seed": rng.randint(0, 2**32 - 1),
+            "wav_file": wav_file,
+        }
