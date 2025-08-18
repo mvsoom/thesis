e39f94370824dcc860f31e297d397210d8c87208
mvsoom <mvsoom@gmail.com>
2025-08-18 11:24:50 +0200

    Reorganize folders

--- notebooks/iklp/test-iklp-I-test.R
+++ notebooks/iklp/test-iklp-I-test.R
@@ -7,15 +7,6 @@ library(ggrepel)
 runs_file <- file.path(Sys.getenv("PROJECT_EXPERIMENTS_PATH"), "iklp-openglot-I-test/runs.csv")
 runs <- data.table(fread(runs_file))
 
-ggplot(runs, aes(x = frame_stats.pitchedness, fill=wav_file)) +
-  stat_bin() +
-  theme_gnuplot()
-
-ggplot(runs, aes(x = frame_stats.pitchedness, y=frame_stats.initial_pitchedness, color=wav_file)) +
-  geom_jitter(width = 0.05, size=4) +
-  theme_gnuplot()
-
-# std
 runs[, pitch_std := sqrt(frame_stats.estimated_pitch_var)]
 runs[, `:=`(pitch_min = frame_stats.estimated_pitch_mean - pitch_std,
            pitch_max = frame_stats.estimated_pitch_mean + pitch_std)]
@@ -23,11 +14,29 @@ runs[, `:=`(pitch_min = frame_stats.estimated_pitch_mean - pitch_std,
 # calculate error
 runs[, pitch_error := abs(frame_stats.estimated_pitch_mean - true_pitch)]
 
-ggplot(runs, aes(x = frame_stats.pitchedness, 
-         y = pitch_error, color=wav_file)) +
+runs[, best_of := frame_stats.score == max(frame_stats.score), by = .(wav_file, frame_stats.frame_index)]
+
+
+# When initial_pitchedness is > 0.5, they all converge to pitchedness of 1
+# When initial_pitchedness is < 0.5, some still converge to pitchedness of 1
+ggplot(runs, aes(x = frame_stats.pitchedness, y = initial_pitchedness, color=wav_file)) +
+geom_jitter(width = 0.05, size=4) +
+  theme_gnuplot()
+
+ggplot(runs, aes(x = initial_pitchedness, 
+         y = pitch_error/true_pitch, color=wav_file, size=best_of, shape=best_of)) +
   geom_jitter() +
   geom_point(size=4) +
+  scale_x_continuous(minor_breaks = waiver()) +
+  scale_y_continuous(minor_breaks = waiver()) +
   theme_gnuplot()
+
+ggplot(runs, aes(x = frame_stats.pitchedness, y=frame_stats.initial_pitchedness, color=wav_file)) +
+  geom_jitter(width = 0.05, size=4) +
+  theme_gnuplot()
+
+# std
+
 
 # Get
 
