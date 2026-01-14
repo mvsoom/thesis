d059b264c0a9d1fe824b053371027c50471eeaf5
mvsoom <mvsoom@gmail.com>
2026-01-14 14:20:07 +0100

    Analyze pack_modal and pmatern_modal

--- experiments/pack_modal/README.md
+++ experiments/pack_modal/README.md
@@ -3,3 +3,18 @@
 Fit PACK: periodic, stationary, isotropic
 
 Optionally allow separate sigma_c's per embedded harmonic.
+
+Note: just 1 iteration for now
+
+# Results
+
+Short walltimes ~ 90 sec with some outliers
+
+- Most score comparably in logz
+- pack:1 is preferred
+- a single sigma_c is sometimes better, sometimes worse => drop it
+- same for normalization => drop it
+
+## vs pmatern_modal
+
+Not enough data; pack:1 and pmatern:32 score comparably with slight preference for latter
---
+++ experiments/pack_modal/stats.R
@@ -0,0 +1,142 @@
+library(ggplot2)
+library(ggnuplot)
+library(data.table)
+library(ggrepel)
+
+# Get experiment dir from environment variable
+runs_file <- file.path(Sys.getenv("PROJECT_EXPERIMENTS_PATH"), "pack_modal/runs.csv")
+runs <- data.table(fread(runs_file))
+
+df <- runs[, .(
+    modality,
+    kernel,
+    J,
+    single_sigma_c,
+    logz,
+    logzerr,
+    information,
+    normalized,
+    iteration,
+    mean.sigma_a_log10,
+    mean.sigma_b_log10,
+    mean.sigma_c_log10,
+    mean.sigma_noise_log10,
+    te
+)]
+
+df[
+    ,
+    `:=`(
+        modality = as.factor(modality),
+        kernel = as.factor(kernel)
+    )
+]
+
+# aggregate over iterations
+df <- df[
+    ,
+    {
+        n <- .N
+        m_logz <- mean(logz)
+        var_mean <- sum(logzerr^2) / (n * n)
+        m_logzerr <- sqrt(var_mean)
+        m_info <- mean(information)
+        m_sigma_a_log10 <- mean(mean.sigma_a_log10)
+        m_sigma_b_log10 <- mean(mean.sigma_b_log10)
+        m_sigma_c_log10 <- mean(mean.sigma_c_log10)
+        m_sigma_noise_log10 <- mean(mean.sigma_noise_log10)
+        list(
+            logz = m_logz,
+            logzerr = m_logzerr,
+            information = m_info,
+            mean.sigma_a_log10 = m_sigma_a_log10,
+            mean.sigma_b_log10 = m_sigma_b_log10,
+            mean.sigma_c_log10 = m_sigma_c_log10,
+            mean.sigma_noise_log10 = m_sigma_noise_log10,
+            te = mean(te)
+        )
+    },
+# styler: off
+    by = .(modality, kernel, J, single_sigma_c, normalized)
+    # styler: on
+]
+
+# set sorting index
+setorder(df, modality, -logz)
+
+# look at results manually (see README.md)
+View(df)
+
+# display top 5 results per modality
+df[
+    ,
+    .SD[1:10],
+    by <- modality
+]
+
+# best kernel per modality by logz
+kernel_scores <- df[
+    ,
+    .(
+        n_modalities = uniqueN(modality),
+        sum_logz = sum(logz),
+        sum_information = sum(information)
+    ),
+    by = .(kernel, J, single_sigma_c, normalized)
+]
+
+kernel_scores <- kernel_scores[n_modalities == 4]
+
+rank_logz <- kernel_scores[
+    order(-sum_logz)
+]
+
+rank_info <- kernel_scores[
+    order(sum_information)
+]
+
+View(rank_logz)
+View(rank_info)
+
+# pareto on normalized
+df[
+    ,
+    `:=`(
+        logz_z = (logz - mean(logz)) / sd(logz),
+        information_z = (information - mean(information)) / sd(information)
+    ),
+    by = modality
+]
+
+kernel_scores <- df[
+    ,
+    .(
+        n_modalities = uniqueN(modality),
+        sum_logz_z = sum(logz_z),
+        sum_information_z = sum(information_z)
+    ),
+    by = .(kernel, J, single_sigma_c, normalized)
+][
+    n_modalities == 4
+]
+
+rank_logz <- kernel_scores[order(-sum_logz_z)]
+rank_info <- kernel_scores[order(sum_information_z)]
+
+View(rank_logz)
+View(rank_info)
+
+setorder(kernel_scores, -sum_logz_z, sum_information_z)
+
+kernel_scores[, best_info := cummin(sum_information_z)]
+
+pareto <- kernel_scores[
+    (seq_len(.N) == 1L) | (sum_information_z < shift(best_info))
+]
+
+pareto[, best_info := NULL]
+
+View(pareto)
+
+# view distribution of walltimes
+summary(runs$walltime)
