85be4b4a5986c7f1d74dac4c1ebfdbde59709d93
mvsoom <mvsoom@gmail.com>
2026-01-27 18:06:27 +0100

    Run lvm/pack experiment

---
+++ experiments/lvm/pack/config.py
@@ -0,0 +1,26 @@
+from itertools import product
+from random import Random
+
+rng = Random(114593)
+
+
+def configurations():
+    for (
+        M,  # number of PRISM basis functions
+        Q,  # latent dimension
+        iteration,
+    ) in product(
+        [8, 16, 32, 64, 128],
+        [1, 3, 6, 9],
+        [1],
+    ):
+        if M < Q:  # require dimensionality reduction
+            continue
+
+        yield {
+            "seed": rng.randint(0, 2**32 - 1),
+            "M": M,
+            "Q": Q,
+            "iteration": iteration,
+            "name": f"M={M}_Q={Q}_iter={iteration}",
+        }
