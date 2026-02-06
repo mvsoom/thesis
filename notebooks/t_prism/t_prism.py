d8bac58bcdde33c8ed01a0f95cd7c6e6b068a85b
mvsoom <mvsoom@gmail.com>
2026-02-06 16:15:46 +0100

    Refactor

--- notebooks/t_prism/t_prism.py
+++ notebooks/t_prism/t_prism.py
@@ -99,8 +99,9 @@ mask = np.random.rand(len(t)) <= 0.5
 key = vk()
 
 # %%
+from prism import svi, t_svi
 
-def collapsed_svi(key=vk(), M=12, jitter=1e-6, lengthscale=1.0, variance=1.0, obs_stddev=1.0):
+def collapsed_svi(key=vk(), nu=1e6, M=12, jitter=1e-6, lengthscale=1.0, variance=1.0, obs_stddev=1.0, num_inner=3):
     Z = init_Z_grid(key, M) * t[-1]
 
     kernel = RBF(lengthscale=lengthscale, variance=variance)
@@ -108,37 +109,35 @@ def collapsed_svi(key=vk(), M=12, jitter=1e-6, lengthscale=1.0, variance=1.0, ob
     likelihood = Gaussian(num_datapoints=len(t), obs_stddev=obs_stddev)
     posterior = prior * likelihood
 
-    return CollapsedVariationalGaussian(
-        posterior=posterior, inducing_inputs=Z, jitter=jitter
+    return t_svi.t_CollapsedVariationalGaussian(
+        posterior=posterior, inducing_inputs=Z, nu=nu, num_inner=num_inner, jitter=jitter
     )
 
-q = collapsed_svi(key, M=12, jitter=1e-6)
-
-
-from prism import svi, t_svi
 
 nu = 1e6
 num_inner = 3
 
+q = collapsed_svi(key, nu=nu, M=12, num_inner=num_inner, jitter=1e-6)
+
+
 elbo = svi.collapsed_elbo_masked(q, t, y)
-elbo_t = t_svi.collapsed_elbo_masked_t(q, t, y, nu, num_inner=num_inner)
+elbo_t = t_svi.t_collapsed_elbo_masked(q, t, y)
 
 print(f"ELBO  : {elbo:.4f}")
 print(f"ELBO_t: {elbo_t:.4f}")
-# these matched before your patches
 
 # Apply a random mask
 t_mask = np.where(mask, t, np.nan)
 y_mask = np.where(mask, y, np.nan)
 
 elbo_m = svi.collapsed_elbo_masked(q, t_mask, y_mask)
-elbo_t_m = t_svi.collapsed_elbo_masked_t(q, t_mask, y_mask, nu, num_inner=num_inner)
+elbo_t_m = t_svi.t_collapsed_elbo_masked(q, t_mask, y_mask)
 
 print(f"ELBO[masked]  : {elbo_m:.4f}")
 print(f"ELBO_t[masked]: {elbo_t_m:.4f}")
 
 mu, Sigma = svi.infer_eps_posterior_single(q, t_mask, y_mask)
-mu_t, Sigma_t = t_svi.infer_eps_posterior_single_t(q, t_mask, y_mask, nu)
+mu_t, Sigma_t, _ = t_svi.t_infer_eps_posterior_single(q, t_mask, y_mask)
 
 print("Posterior mean max abs diff:", np.max(np.abs(mu - mu_t)))
 print("Posterior cov max abs diff :", np.max(np.abs(Sigma - Sigma_t)))
@@ -149,12 +148,14 @@ print("Posterior cov max abs diff :", np.max(np.abs(Sigma - Sigma_t)))
 # Compare posteriors
 
 # tune to data manually
-q = collapsed_svi(key, M=12, jitter=1e-6, lengthscale=10.0, variance=1.0, obs_stddev=0.01)
-
 nu = 1.0
 
+
+q = collapsed_svi(key, nu=nu, M=12, jitter=1e-6, lengthscale=10.0, variance=1.0, obs_stddev=0.01)
+
+
 mu, Sigma = svi.infer_eps_posterior_single(q, t, y)
-mu_t, Sigma_t = t_svi.infer_eps_posterior_single_t(q, t, y, nu)
+mu_t, Sigma_t, w = t_svi.t_infer_eps_posterior_single(q, t, y)
 
 from prism.svi import gp_posterior_mean_from_eps
 
@@ -166,5 +167,9 @@ y_star_t = gp_posterior_mean_from_eps(q, t, mu_t)
 plot(t, y_star, label="posterior mean")
 plot(t, y_star_t, "--", label="posterior mean t")
 legend()
+show()
 
+# weights allow to score points
+plot(w)
+yscale("log") 
 # %%
---
+++ src/aplawd/data.py
@@ -0,0 +1,73 @@
+# %%
+import numpy as np
+
+from aplawd import APLAWD_Markings
+from prism.svi import pad_waveforms
+from utils import __datadir__
+
+
+def get_list_of_periods():
+    period_list = []
+
+    markings_db = APLAWD_Markings(__datadir__("APLAWDW/markings/aplawd_gci"))
+
+    for key in markings_db.keys():
+        m = markings_db.load(key)
+        period_ms = np.diff(m) / 20000 * 1000
+        period_list.append(period_ms)
+
+    return period_list
+
+
+def get_data_periods(n=None, offset=0, width=None, dtype=np.float64):
+    if n is None:
+        period_list = get_list_of_periods()[offset:]
+    else:
+        period_list = get_list_of_periods()[offset : offset + n]
+    indices_list = [np.arange(len(p)) for p in period_list]
+
+    pairs = list(zip(indices_list, period_list))
+
+    X, y = pad_waveforms(pairs, width=width, dtype=dtype)
+    return X, y
+
+
+def get_whitener(y):
+    mean = np.nanmean(y)
+    std = np.nanstd(y)
+
+    def whiten(y):
+        return (y - mean) / std
+
+    def unwhiten(y):
+        return y * std + mean
+
+    return whiten, unwhiten
+
+
+if __name__ == "__main__":
+    X, y = get_data_periods()
+
+    # Choose a width for easier inference time
+    lens = np.sum(~np.isnan(y), axis=1)
+
+    print(f"Number of waveforms: {len(y)}")
+    print(f"Average length: {np.mean(lens):.1f} samples")
+    print(f"Max length: {np.max(lens)} samples")
+    print(f"Min length: {np.min(lens)} samples")
+
+    print("95% quantile of length:", np.quantile(lens, 0.95))
+    print(
+        "99% quantile of length:", np.quantile(lens, 0.99)
+    )  # cut at width=320
+
+    # We know physiological cutoff at ~50 Hz
+    # This corresponds to 99% quantile
+    p99 = np.nanquantile(y, 0.99)
+    f0_cutoff = 1000 / p99
+    print(f"F0 cutoff (99% quantile): {f0_cutoff:.2f} Hz")
+
+    print("Number of events:", np.sum(~np.isnan(y)))
+    print(
+        "Events > cutoff (99%):", np.sum(y >= p99)
+    )  # 1% => implies value of nu in t-PRISM
